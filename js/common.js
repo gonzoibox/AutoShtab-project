3d4312721f6251967be143f47e619b597e5a7c9f
anton_davidovich <dvd14@ukr.net>
2018-12-12 23:25:55 +0200

    add a map code

--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
-$(function() {
 
-	// Custom JS
-
-});
+function initMap() {
+	// The location of Uluru
+	var uluru = {lat: 56.866558, lng: 53.209415};
+	// The map, centered at Uluru
+	var map = new google.maps.Map(
+		document.getElementById('map'), {zoom: 4, center: uluru});
+	// The marker, positioned at Uluru
+	var marker = new google.maps.Marker({position: uluru, map: map});
+  }
